30c7f448bc29fb4fe6b61a3ab1b4beedc0277c1d
shiv singh <shiv007singh@gmail.com>
2019-02-20 09:56:46 +0530

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Welome to my starter App</h1>
+            </div>
+        )
+    }
+};
+
+export default App;
---
+++ test/App.test.js
@@ -0,0 +1,13 @@
+import App from '../src/components/App';
+import React from 'react';
+import {
+    shallow
+} from 'enzyme';
+
+describe('App', () => {
+    test('show match snapshot', () => {
+        const wrapper = shallow( < App /> )
+        expect(wrapper.find('h1').text()).toBe('Welome to my starter App')
+        expect(wrapper).toMatchSnapshot
+    })
+})
